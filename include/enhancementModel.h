0636c6e57ffb870aa1e8647dcbafad3f797b29a9
Karthik Mohan Kumar <kmohanku@ucsc.edu>
2021-01-14 11:36:45 -0600

    Fixed more formatting issues

--- include/enhancementModel.h
+++ include/enhancementModel.h
@@ -10,20 +10,17 @@
 
 class EnhancementModel{
 	private:
-
 	const std::string model_path;
     	const std::string input_node;
     	const std::string output_node;
     	const float max_pixel = 255.0;
-
-		tensorflow::SavedModelBundleLite model_bundle;
+	
+	tensorflow::SavedModelBundleLite model_bundle;
     	tensorflow::SessionOptions session_options = tensorflow::SessionOptions();
     	tensorflow::RunOptions run_options = tensorflow::RunOptions();
-
     	void enhanceImage(std::vector<tensorflow::Tensor>&, std::vector<tensorflow::Tensor>*);
 
     public:
-
     	EnhancementModel(const std::string&, const std::string&, const std::string&);
     	tensorflow::Tensor convertMatToTensor(const cv::Mat&);
     	cv::Mat runModel(const cv::Mat&);
